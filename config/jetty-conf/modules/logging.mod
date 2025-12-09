703032f8172f3a82f78d10c6b9e52484b9a707a8
root <root@shamrock-dev-environment-service-v1-6d7785c75f-gzq8t>
2025-12-09 11:56:42 +0000

    Initial commit

---
+++ config/jetty-conf/modules/logging.mod
@@ -0,0 +1,16 @@
+#
+# Jetty Logging Module
+# Output Managed by logback or log4j over Slf4j
+#
+
+[tags]
+logging
+
+[depend]
+resources
+
+[lib]
+lib/logging/*.jar
+
+[files]
+logs/
---
+++ modules/core/src/main/java/com/haulmont/shamrock/mule/netquall/adapter/ServiceConfiguration.java
@@ -0,0 +1,10 @@
+package com.haulmont.shamrock.mule.netquall.adapter;
+
+import com.haulmont.monaco.config.annotations.Config;
+import com.haulmont.monaco.config.annotations.Property;
+import org.picocontainer.annotations.Component;
+
+@Config
+@Component
+public interface ServiceConfiguration {
+}
