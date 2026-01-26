88f104a08e2a48a49cce7eec0de559ffa271e649
Eugene Yakimenko <yakimenko@haulmont.com>
2026-01-26 15:09:45 +0400

    #1 SM-815 NetQual (inbound)

--- modules/core/src/main/java/com/haulmont/shamrock/affiliate/netquall/dto/req/NetquallCallbackRequest.java
+++ modules/core/src/main/java/com/haulmont/shamrock/affiliate/netquall/dto/req/NetquallCallbackRequest.java
@@ -7,7 +7,19 @@
 package com.haulmont.shamrock.affiliate.netquall.dto.req;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.haulmont.shamrock.affiliate.netquall.dto.NetquallResponseMessage;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
-public class NetquallCallbackRequest extends NetquallBaseRequest{
+public class NetquallCallbackRequest extends NetquallBaseRequest {
+    @JsonProperty("ResponseData")
+    private NetquallResponseMessage responseData;
+
+    public NetquallResponseMessage getResponseData() {
+        return responseData;
+    }
+
+    public void setResponseData(NetquallResponseMessage responseData) {
+        this.responseData = responseData;
+    }
 }
